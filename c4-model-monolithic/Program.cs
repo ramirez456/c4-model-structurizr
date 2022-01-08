0be41e9a9b268c643ed71181cd6c272c6aed4254
Max Houston Ramirez Martel <maxr524@gmail.com>
2022-01-08 08:28:44 -0500

    examen

--- c4-model-monolithic/Program.cs
+++ c4-model-monolithic/Program.cs
@@ -17,21 +17,23 @@ namespace c4_model_monolithic
             const string apiSecret = "96576e59-fca9-4b3e-89f5-2ee6f23c0cf5";
 
             StructurizrClient structurizrClient = new StructurizrClient(apiKey, apiSecret);
-            Workspace workspace = new Workspace("C4 Ing. Software", "sistema de asistencia");
+            Workspace workspace = new Workspace("C4 Ing. Software examen final", "sistema de veterinaria");
             ViewSet viewSet = workspace.Views;
             Model model = workspace.Model;
 
             // 1. Diagrama de Contexto
-            SoftwareSystem monitoringSystem = model.AddSoftwareSystem("Sistema de registro de asistencias", "Permite el registro de participantes, registro de sus asistencias y la generacion de sus certificados");
+            SoftwareSystem monitoringSystem = model.AddSoftwareSystem("Sistema de gestion de citas, ", "Sistema que permite generar citas y registro de los clientes y pacientes(mascotas) ");
 
-            Person ponente = model.AddPerson("Ponente", "Ponente");
-            Person organizador = model.AddPerson("Organizador", "encargado");
-            Person participante = model.AddPerson("Participante", "Persona");
+            Person cliente = model.AddPerson("Cliente", "Cliente");
+            Person macotas = model.AddPerson("Mascotas", "Los pacientes");
+            Person usuario = model.AddPerson("Usuario", "Persona que usa el sistema");
+            Person doctor = model.AddPerson("Doctor", "Doctor que atienden al paciente");
             
             
-            ponente.Uses(monitoringSystem, "Obtiene certificación");
-            organizador.Uses(monitoringSystem, "Registra el evento, inscribe perticipantes, gestiona las encuestas y emite certificados.");
-            participante.Uses(monitoringSystem, "Registra su participación, obtiene su certificado, rellena encuentas de la ponencia");
+            cliente.Uses(monitoringSystem, "LLevan a las macostas a ser atendidas");
+            macotas.Uses(monitoringSystem, "Tiene un historial de atenciones");
+            usuario.Uses(monitoringSystem, "Registras todas las atenciones y intervenciones del paciente y lo enlaza con la cuenta del cliente");
+            doctor.Uses(monitoringSystem, "Atiende a los pacientes y los se lo comunica al usuario para que registro lo atendido");
             
             SystemContextView contextView = viewSet.CreateSystemContextView(monitoringSystem, "Contexto", "Diagrama de contexto");
             contextView.PaperSize = PaperSize.A4_Landscape;
@@ -40,32 +42,40 @@ namespace c4_model_monolithic
 
             // Tags
             
-            ponente.AddTags("Ponente");
-            organizador.AddTags("Organizador");
-            participante.AddTags("Participante");
+            cliente.AddTags("Ponente");
+            doctor.AddTags("Doctor");
+            macotas.AddTags("Organizador");
+            usuario.AddTags("Participante");
             monitoringSystem.AddTags("SistemaMonitoreo");
 
             Styles styles = viewSet.Configuration.Styles;
             styles.Add(new ElementStyle("Participante") { Background = "#6194FA", Color = "#ffffff", Shape = Shape.Person });
             styles.Add(new ElementStyle("Organizador") { Background = "#AF1BFF", Color = "#ffffff", Shape = Shape.Person });
             styles.Add(new ElementStyle("Ponente") { Background = "#FF2641", Color = "#ffffff", Shape = Shape.Person });
+            styles.Add(new ElementStyle("Doctor") { Background = "#008000", Color = "#ffffff", Shape = Shape.Person });
             styles.Add(new ElementStyle("SistemaMonitoreo") { Background = "#79E058", Color = "#ffffff", Shape = Shape.RoundedBox });
 
             // 2. Diagrama de Contenedores
             Container webApplication = monitoringSystem.AddContainer("Web App", "Permite a los organizadores registrar las asistencias, registrar participantes y generar los certificados.", "Flutter Web");
-            Container landingPage = monitoringSystem.AddContainer("Landing Page", "Permite a los usuarios ver las ponencias disponibles", "Flutter Web");
+            Container MobileApplication = monitoringSystem.AddContainer("Mobile App", "Permite al cliente elegir a su doctor favorito para su mascota", "Flutter Web");
+            Container landingPage = monitoringSystem.AddContainer("Landing Page", "Permite a los clientes ver los servicios disponibles", "Flutter Web");
             Container apiRest = monitoringSystem.AddContainer("API Rest", "API Rest", "NodeJS (NestJS) port 8080");
-            Container certificateContext = monitoringSystem.AddContainer("Certificate Context", "Bounded Context de certificados", "NodeJS (NestJS)");
-            Container participanteContext = monitoringSystem.AddContainer("Participante Context", "Bounded Context de Registro de participantes", "NodeJS (NestJS)");
-            Container asistenciaContext = monitoringSystem.AddContainer("Assistance Context", "Bounded Context de registro de asistencias", "NodeJS (NestJS)");
-            Container eventContext = monitoringSystem.AddContainer("Event Context", "Bounded Context de registro de eventoos", "NodeJS (NestJS)");
+            Container certificateContext = monitoringSystem.AddContainer("Citas Context", "Bounded Context de gestion de citas", "NodeJS (NestJS)");
+            Container participanteContext = monitoringSystem.AddContainer("Historial Medico Context", "Bounded Context de Registro de historial medico", "NodeJS (NestJS)");
+            Container asistenciaContext = monitoringSystem.AddContainer("Recetas Context", "Bounded Context de registro de recetas", "NodeJS (NestJS)");
+            Container eventContext = monitoringSystem.AddContainer("Pacientes Context", "Bounded Context de registro de las mascotas", "NodeJS (NestJS)");
+            Container facturacionContext = monitoringSystem.AddContainer("Facturacion Context", "Bounded Context de registro de los comprobantes", "NodeJS (NestJS)");
             Container database = monitoringSystem.AddContainer("Database", "", "Oracle");
             
-            participante.Uses(landingPage, "Consulta");
-            ponente.Uses(landingPage, "Consulta");
-            organizador.Uses(webApplication, "Gestiona");
+            usuario.Uses(webApplication, "Gestiona");
+            cliente.Uses(landingPage, "Consulta");
+            macotas.Uses(landingPage, "consulta");
+            cliente.Uses(MobileApplication, "Consulta");
+            macotas.Uses(MobileApplication, "consulta");
+            doctor.Uses(webApplication, "Gestiona");
 
             landingPage.Uses(apiRest, "API Request", "JSON/HTTPS");
+            MobileApplication.Uses(apiRest, "API Request", "JSON/HTTPS");
             webApplication.Uses(apiRest, "API Request", "JSON/HTTPS");
 
             
@@ -73,23 +83,27 @@ namespace c4_model_monolithic
             apiRest.Uses(participanteContext, "", "");
             apiRest.Uses(asistenciaContext, "", "");
             apiRest.Uses(eventContext, "", "");
+            apiRest.Uses(facturacionContext, "", "");
             
             
             certificateContext.Uses(database, "", "JDBC");
             participanteContext.Uses(database, "", "JDBC");
             asistenciaContext.Uses(database, "", "JDBC");
             eventContext.Uses(database, "", "JDBC");
+            facturacionContext.Uses(database, "", "JDBC");
 
             // Tags
             webApplication.AddTags("WebApp");
             landingPage.AddTags("LandingPage");
             apiRest.AddTags("APIRest");
             database.AddTags("Database");
-            
+            MobileApplication.AddTags("MobileApp");
             certificateContext.AddTags("CertificateContext");
             participanteContext.AddTags("ParticipanteContext");
             asistenciaContext.AddTags("AsistenciaContext");
             eventContext.AddTags("EventContext");
+            facturacionContext.AddTags("EventContext");
+
 
             styles.Add(new ElementStyle("MobileApp") { Background = "#9d33d6", Color = "#ffffff", Shape = Shape.MobileDevicePortrait, Icon = "" });
             styles.Add(new ElementStyle("WebApp") { Background = "#9d33d6", Color = "#ffffff", Shape = Shape.WebBrowser, Icon = "" });
@@ -104,77 +118,6 @@ namespace c4_model_monolithic
             ContainerView containerView = viewSet.CreateContainerView(monitoringSystem, "Contenedor", "Diagrama de contenedores");
             contextView.PaperSize = PaperSize.A4_Landscape;
             containerView.AddAllElements();
-
-            // 3. Diagrama de Componentes asistencias
-            Component domainLayer = asistenciaContext.AddComponent("Domain Layer", "", "NodeJS (NestJS)");
-            Component registerController = asistenciaContext.AddComponent("Register Controller", "REST API endpoints de registro.", "NodeJS (NestJS) REST Controller");
-            Component assistanceApplicationService = asistenciaContext.AddComponent("Assistance Application Service", "Provee métodos para la gestion, pertenece a la capa Application de DDD", "NestJS Component");
-            Component asistenciaEntity = asistenciaContext.AddComponent("Assistance Entity", "model para  Asistencias", "NestJS Component");
-            Component asistenciaMaper = asistenciaContext.AddComponent("Assistance Maper", "Tipado de las request", "NestJS Component");
-
-            apiRest.Uses(registerController, "", "JSON/HTTPS");
-            registerController.Uses(assistanceApplicationService, "Invoca métodos de registro");
-            assistanceApplicationService.Uses(domainLayer, "Usa", "");
-            assistanceApplicationService.Uses(asistenciaEntity, "", "JDBC");
-            assistanceApplicationService.Uses(asistenciaMaper, "", "JDBC");
-            asistenciaEntity.Uses(database, "", "JDBC");
-            asistenciaMaper.Uses(database, "", "JDBC");
-            
-            // Tags
-            domainLayer.AddTags("DomainLayer");
-            registerController.AddTags("RegisterController");
-            assistanceApplicationService.AddTags("assistanceApplicationService");
-            asistenciaEntity.AddTags("AsistenciaEntity");
-            asistenciaMaper.AddTags("AsistenciaMaper");
-            
-            styles.Add(new ElementStyle("DomainLayer") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
-            styles.Add(new ElementStyle("RegisterController") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
-            styles.Add(new ElementStyle("assistanceApplicationService") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
-            styles.Add(new ElementStyle("MonitoringDomainModel") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
-            styles.Add(new ElementStyle("AsistenciaEntity") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
-            styles.Add(new ElementStyle("AsistenciaMaper") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
-
-            ComponentView componentView = viewSet.CreateComponentView(asistenciaContext, "Component", "Component Diagram");
-            componentView.PaperSize = PaperSize.A4_Landscape;
-            componentView.Add(webApplication);
-            componentView.Add(apiRest);
-            componentView.Add(database);
-            componentView.AddAllComponents();
-
-            //4.- componentes eventos
-            Component domainEventLayer = eventContext.AddComponent("Domain Layer", "", "NodeJS (NestJS)");
-            Component eventController = eventContext.AddComponent("Register Controller", "REST API endpoints de registro.", "NodeJS (NestJS) REST Controller");
-            Component eventApplicationService = eventContext.AddComponent("Event Application Service", "Provee métodos para la gestion, pertenece a la capa Application de DDD", "NestJS Component");
-            Component eventEntity = eventContext.AddComponent("Event Entity", "model para  event", "NestJS Component");
-            Component eventMaper = eventContext.AddComponent("Event Maper", "Tipado de las request", "NestJS Component");
-
-            apiRest.Uses(eventController, "", "JSON/HTTPS");
-            eventController.Uses(eventApplicationService, "Invoca métodos de registro");
-            eventApplicationService.Uses(domainEventLayer, "Usa", "");
-            eventApplicationService.Uses(eventEntity, "", "JDBC");
-            eventApplicationService.Uses(eventMaper, "", "JDBC");
-            eventEntity.Uses(database, "", "JDBC");
-            eventMaper.Uses(database, "", "JDBC");
-            
-            // Tags
-            domainEventLayer.AddTags("domainEventLayer");
-            eventController.AddTags("eventController");
-            eventApplicationService.AddTags("eventApplicationService");
-            eventEntity.AddTags("eventEntity");
-            eventMaper.AddTags("eventMaper");
-            
-            styles.Add(new ElementStyle("domainEventLayer") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
-            styles.Add(new ElementStyle("eventController") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
-            styles.Add(new ElementStyle("eventApplicationService") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
-            styles.Add(new ElementStyle("eventEntity") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
-            styles.Add(new ElementStyle("eventMaper") { Shape = Shape.Component, Background = "#facc2e", Icon = "" });
-
-            ComponentView componentEventView = viewSet.CreateComponentView(eventContext, "Components", "Component Diagram");
-            componentEventView.PaperSize = PaperSize.A4_Landscape;
-            componentEventView.Add(webApplication);
-            componentEventView.Add(apiRest);
-            componentEventView.Add(database);
-            componentEventView.AddAllComponents();
 
             structurizrClient.UnlockWorkspace(workspaceId);
             structurizrClient.PutWorkspace(workspaceId, workspace);
